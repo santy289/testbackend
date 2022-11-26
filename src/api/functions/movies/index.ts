1afdb90de4a9da120d90ca4682692a382ebe072d
Santy Dev <devsanty@icloud.com>
2022-11-26 10:48:00 -0500

    Fixes

--- src/api/functions/movies/index.ts
+++ src/api/functions/movies/index.ts
@@ -1,9 +1,14 @@
 const { Router } = require('express');
-import { getPopular, getTop } from '../../services/movieHandler';
+import {
+  getPopular,
+  getTop,
+  getUpcoming,
+} from '../../services/movieHandler';
 
 const router = Router();
 
 router.get('/popular/:page', getPopular)
 router.get('/topRated/:page', getTop)
+router.get('/upcoming/:page', getUpcoming)
 
 module.exports = router;
--- src/api/services/movieHandler.ts
+++ src/api/services/movieHandler.ts
@@ -8,7 +8,6 @@ import {
 const url = 'https://api.themoviedb.org/3';
 
 export const getPopular: any = async (req: reqShema, res: resSchema) => {
-  console.log(req.params);
   const page = req.params.page;
   fetch(`${url}/movie/popular?api_key=${process.env.MOVIE_DATABASE_API_KEY}&page=${page}`, {
     method: "GET",
@@ -26,7 +25,6 @@ export const getPopular: any = async (req: reqShema, res: resSchema) => {
   };
 
   export const getTop: any = async (req: reqShema, res: resSchema) => {
-    console.log(req.params);
     const page = req.params.page;
     fetch(`${url}/movie/top_rated?api_key=${process.env.MOVIE_DATABASE_API_KEY}&page=${page}`, {
       method: "GET",
@@ -44,7 +42,6 @@ export const getPopular: any = async (req: reqShema, res: resSchema) => {
   };
 
   export const getUpcoming: any = async (req: reqShema, res: resUpcomingSchema) => {
-    console.log(req.params);
     const page = req.params.page;
     fetch(`${url}/movie/upcoming?api_key=${process.env.MOVIE_DATABASE_API_KEY}&page=${page}`, {
       method: "GET",
